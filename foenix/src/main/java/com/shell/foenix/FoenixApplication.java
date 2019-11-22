3134fb0bf10e72c688f48634faad41917943a47d
aditya <foenix>
2019-11-22 11:59:41 +0530

    Docker file

--- foenix/src/main/java/com/shell/foenix/FoenixApplication.java
+++ foenix/src/main/java/com/shell/foenix/FoenixApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
+
+
 @SpringBootApplication
 public class FoenixApplication extends SpringBootServletInitializer {
 
